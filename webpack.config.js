ad1096c281c97982b1df38ffaa8e4b5c66fccced
Herson Castillo <hersoncastillo12@hotmail.com>
2021-03-23 22:55:26 -0600

    :fire: Webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,42 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const environment = process.env.NODE_ENV || 'dev';
 const isDevelopment = environment !== 'prod';
 
 module.exports = {
   mode: isDevelopment ? 'development' : 'production',
-  entry: './dist/main.js',
-  watch: isDevelopment,
+  entry: './src/main.ts',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'public'),
+    filename: isDevelopment ? '[id].bundle.js' : '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 9000,
+    overlay: {
+      warnings: true,
+      errors: true,
+    },
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+    },
   },
   resolve: {
     extensions: ['.js', '.ts', '.d.ts'],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.join(__dirname, 'public', 'index.html'),
+      chunks: 'all',
+      minify: !isDevelopment,
+    }),
+  ],
   module: {
     rules: [
       {
@@ -24,7 +47,10 @@ module.exports = {
       {
         test: /\.ts|.tsx$/,
         exclude: /node_modules/,
-        loader: 'ts-loader'
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true
+        }
       },
     ],
   },
